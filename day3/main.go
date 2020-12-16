07da2bf49e251243a7ee0280e66499a500ab5b29
Delia Popescu <dpopescu@fitbit.com>
2020-12-16 17:54:22 +0200

    Add day3 part 2

--- day3/main.go
+++ day3/main.go
@@ -31,31 +31,61 @@ func ReadFile(arg string) (data []string) {
 	return data
 }
 
-func CoundTrees(data []string) (err error, trees int){
+func CoundTrees(data []string) (err error, totalSlope int){
 	count := 0
-	pozition := 1
+	pozitionSlope1 := 1
+	pozitionSlope2 := 1
+	pozitionSlope3 := 1
+	pozitionSlope4 := 1
+	pozitionSlope5 := 1
 	total_lines := len(data)
-	trees = 0
+	treesSlope1 := 0
+	treesSlope2 := 0
+	treesSlope3 := 0
+	treesSlope4 := 0
+	treesSlope5 := 0
+	countSlope5 := 1
+	tree := "#"
 
 	for _, line := range data{
 		count += 1
-
-
 		val := strings.Split(line, "")
 
-		for len(val) <= pozition {
+		for len(val) <= pozitionSlope2 || len(val) <= pozitionSlope1 || len(val) <= pozitionSlope3 || len(val) <= pozitionSlope4{
 			val = append(val, val...)
 		}
-		if val[pozition-1] == "#" {
-			trees += 1
+		if val[pozitionSlope1-1] == tree {
+			treesSlope1 += 1
+		}
+		if val[pozitionSlope2-1] == tree {
+			treesSlope2 += 1
+		}
+		if val[pozitionSlope3-1] == tree {
+			treesSlope3 += 1
+		}
+		if val[pozitionSlope4-1] == tree {
+			treesSlope4 += 1
+		}
+		if count == countSlope5{
+			if val[pozitionSlope5-1] == tree {
+				treesSlope5 += 1
+			}
+			pozitionSlope5 += 1
+			countSlope5 += 2
 		}
 
 		if total_lines != count {
 			fmt.Errorf("Failed to count all lines")
 		}
-		pozition = pozition + 3
+
+		pozitionSlope1 += 1
+		pozitionSlope2 += 3
+		pozitionSlope3 += 5
+		pozitionSlope4 += 7
+
 
 	}
-	return err, trees
+	fmt.Printf("%d, %d, %d, %d, %d\n", treesSlope1, treesSlope2, treesSlope3, treesSlope4, treesSlope5)
+	return err, treesSlope1*treesSlope2*treesSlope3*treesSlope4*treesSlope5
 
 }
